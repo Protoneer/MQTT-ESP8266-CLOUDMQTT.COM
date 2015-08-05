b7bafc8bd8527af90d9fe350e90abfc07b5b9d0e
Bertus Kruger <krugercreations@gmail.com>
2015-08-05 21:36:55 +1200

    Update DHT.ino

--- examples/HumidityTempSensor/DHT.ino
+++ examples/HumidityTempSensor/DHT.ino
@@ -1,20 +1,22 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include "DHT.h"
+
 
 const char *ssid =  "XXXX";   // cannot be longer than 32 characters!
 const char *pass =  "XXXX";   //
 
 const char *mqtt_server = "XXXX";
-const int mqtt_port = XXXX;
+const int mqtt_port = 0;
 const char *mqtt_user = "XXXX";
 const char *mqtt_pass = "XXXX";
-const char *mqtt_client_name = "XXXX";
+const char *mqtt_client_name = "XXXX"; // Client connections cant have the same connection name
 
-
-
-#define BUFFER_SIZE 100
-
-bool LedState = false;
+const int dhtPin = 2;
+#define DHTTYPE DHT22
+DHT dht(dhtPin, DHTTYPE, 12); // The 12 might need tuning so play with 10-15 till it works for the ESP8266
+unsigned long previousMillis = 0;
+const long interval = 10000;   
 
 
 WiFiClient wclient;
@@ -26,7 +28,8 @@ void setup() {
   delay(10);
   Serial.println();
   Serial.println();
-  pinMode(0, OUTPUT);
+
+  dht.begin();
 }
 
 void loop() {
@@ -55,8 +58,33 @@ void loop() {
     if (client.connected())
       client.loop();
   }
+
+  SendTempHumid();
 }
 
+// Non-Blocking delay
 void SendTempHumid(){
+  unsigned long currentMillis = millis();
+ 
+  if(currentMillis - previousMillis >= interval) {
+    // save the last time you read the sensor 
+    previousMillis = currentMillis;   
+ 
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+
+    if (isnan(h) || isnan(t)) {
+      Serial.println("Failed to read from DHT sensor!");
+      return;
+    }
+
+    Serial.print("Humidity: "); 
+    Serial.print(h);
+    Serial.print(" %\t");
+    Serial.print("Temperature: "); 
+    Serial.print(t);
+    Serial.println(" *C ");
   
+    client.publish("/test/DHT",String(h)+"%"+String(t)+"C" );
+  }
 }
